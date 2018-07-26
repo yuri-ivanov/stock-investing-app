2a2c7b22d90cf4a944e3e787d2ca691b604e83b7
Yuri Ivanov <iurii.ivanov@moller.no>
2018-07-26 15:33:15 +0200

    add static files

--- src/main/java/com/stockinvest/server/StockInvestApp.java
+++ src/main/java/com/stockinvest/server/StockInvestApp.java
@@ -6,11 +6,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Service;
 
 @SpringBootApplication(scanBasePackages = { "com.stockinvest"}  )
-public class ServerApplication {
+public class StockInvestApp {
 
 	@Value("${useMockData}")
 	private Boolean useMockData;
@@ -18,12 +16,12 @@ public class ServerApplication {
 	@Bean
 	public StockPriceDataSource stockPriceDataSource(){
 		if(useMockData){
-			return new NetfondsDataSource(ServerApplication.class.getClassLoader().getResource("nas-20180627.csv").toString());
+			return new NetfondsDataSource(StockInvestApp.class.getClassLoader().getResource("nas-20180627.csv").toString());
 		}
 		return new NetfondsDataSource();
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+		SpringApplication.run(StockInvestApp.class, args);
 	}
 }
